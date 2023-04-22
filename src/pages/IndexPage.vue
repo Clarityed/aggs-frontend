bc12a2259fd819e31b455fd02d88d83cb6db8f8b
clarity <2270893459@qq.com>
2023-04-22 10:09:43 +0800

    前端对接后端实现类型查询聚合接口

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="index-page">
     <a-input-search
-      v-model:value="searchParams.text"
+      v-model:value="searchText"
       placeholder="input search text"
       enter-button="Search"
       size="large"
@@ -30,14 +30,17 @@ import UserList from "@/pages/UserList.vue";
 import MyDivider from "@/pages/MyDivider.vue";
 import { useRoute, useRouter } from "vue-router";
 import myAxios from "@/plugins/myAxios";
+import { message } from "ant-design-vue";
 
 /* route 取出 url 里面的参数 */
 const route = useRoute();
 /* router 跳转传递参数 */
 const router = useRouter();
 const activeKey = route.params.category;
+const searchText = ref(route.query.text || "");
 
 const initSearchParams = {
+  type: activeKey,
   text: "",
   pageSize: 10,
   pageNum: 1,
@@ -45,44 +48,53 @@ const initSearchParams = {
 
 const searchParams = ref(initSearchParams);
 
-/* 理论上只要这里面监听的参数改变，就会同步刷新 */
-watchEffect(() => {
-  searchParams.value = {
-    ...initSearchParams,
-    text: route.query.text as string,
-  } as never;
-});
-
 const postList = ref([]);
 const userList = ref([]);
 const pictureList = ref([]);
 
 /**
- * 加载数据
+ * 加载单类型数据
  * @param params
  */
 const loadDate = (params: any) => {
+  const { type } = params;
+  if (!type) {
+    message.error("类别为空");
+    return;
+  }
   /* 请求后端接口 */
   const query = {
     ...params,
     searchText: params.text,
   };
   myAxios.post("search/aggs", query).then((res: any) => {
-    postList.value = res.postVOList;
-    userList.value = res.userVOList;
-    pictureList.value = res.pictureList;
+    if (type === "post") {
+      postList.value = res.dataList;
+    } else if (type === "user") {
+      userList.value = res.dataList;
+    } else if (type === "picture") {
+      pictureList.value = res.dataList;
+    }
   });
 };
 
-/* 加载页面时首先就执行一次 */
-loadDate(initSearchParams);
+/* 理论上只要这里面监听的参数改变，就会同步刷新 */
+watchEffect(() => {
+  searchParams.value = {
+    ...initSearchParams,
+    text: route.query.text,
+    type: route.params.category,
+  } as never;
+  loadDate(searchParams.value);
+});
 
 const onSearch = (value: string) => {
-  console.log(value);
   router.push({
-    query: searchParams.value,
+    query: {
+      ...searchParams.value,
+      text: value,
+    },
   });
-  loadDate(searchParams.value);
 };
 
 const onTabSearch = (key: string) => {
